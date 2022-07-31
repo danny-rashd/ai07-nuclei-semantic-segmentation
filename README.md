b5fc3043c380a751fefe7455121d821692c9de59
danny-rashd <danny.rashd@gmail.com>
2022-07-31 23:53:31 +0800

    Add images in public, minor changes for README.md and main.py

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# ai07-nuclei-semantic-segmentation
- Semantic segmentation for images contraining cell nuclei (Data Science Bowl 2018)
+# Nuclei Semantic Segmentation using TensorFlow (ResNet50)
+
+## 1. Summary
+The aim of this project is to create a model that can perform image segmentation on images containing cell nuclei (Data Science Bowl 2018).
+
+## 2. IDE and Frameworks used
+The project is built using the Spyder and Visual Studio Code IDEs for the python (.py) and jupyter notebook (.ipynb) respectively.
+
+The python packages used in this project is [TensorFlow](tensorflow.org) [Keras](https://keras.io/about/), [Matplotlib](https://matplotlib.org/) and [NumPy](https://numpy.org/).
+
+## 3. Methodology
+
+### 3.1 About the Dataset
+
+The dataset contains 2 folders of images of cell nuclei which images and masks as the inputs and labels respectively. 
+
+Each folder has 670 images with a total of 1340 images with 256 x 256 pixels. The [dataset](https://www.kaggle.com/c/data-science-bowl-2018) was obtained from Kaggle. The sample of the training datasets are shown below:
+
+Images
+
+![Images examples](public/input_images.png)
+
+Masks
+![Masks examples](public/masks_images.png)
+
+Input Image and True Mask side-by-side
+
+![Input vs Masks examples](public/input_vs_true_mask.png)
+
+### 3.2 Data Pipeline
+From the dataset, the training and test data set is split into an 80:20 ratio. I also resized the images to 128 x 128 x 3 (3 represents the number of channels in a RGB image).
+
+In the data augmentation layer, transformations are applied the training images using horizontal and vertical flipping to expose the model to different aspects of the training images and reduce overfitting   .          
+
+To construct the U-Net model, I created a base model from MobileNetV2 and excluded the top layer for classification layer. I also used pix2pix as the upsampling path and froze the downsampling path before compiling and training.
+
+In the classification layer, I applied a global average pooling layer and a Dense layer of 2 outputs (2 class - 'Negative' and 'Positive') and softmax as the activation function.
+
+### 3.3 Model Structure
+For this project, I used MobileNetV2 as the encoder for the U-Net due lightweight and fast model. The structure of the image segmentation model is as shown below:
+
+![Model structure](public/model_plot.png)
+
+The model was trained with a batch size of 32 and for 10 epochs and ended with a training accuracy of 96.1% as shown in the image below:
+
+![Model training](public/model_training.png)
+
+## 4. Model evaluation
+With test images, the model obtained the following results during model inference:
+
+![Test Results](public/inference_1.png)
+
+![Test Results](public/inference_2.png)
+
+![Test Results](public/inference_3.png)
+
+# License
+Distributed under GNU General Public License v3.0. See [LICENSE](LICENSE) for more information.
+
+## Acknowledgements
+-  [TensorFlow - Image Classification](https://www.tensorflow.org/tutorials/images/classification)
+-  [MobileNetV2 vs ResNet50](https://analyticsindiamag.com/mobilenet-vs-resnet50-two-cnn-transfer-learning-light-frameworks/#:~:text=As%20we%20can%20see%20in,accuracy%2065%25%20in%20100%20epochs.)
+-  Kah Chun Kong - SHRDC Technical Instructor [![Github](https://img.shields.io/badge/Github-171515?style=flat-square&logo=github&logoColor=black)](https://github.com/ch4mploo/)
--- main.py
+++ main.py
@@ -5,25 +5,22 @@ Created on Wed Jul 27 15:57:36 2022
 @author: User
 """
 # Import packages
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers,losses, optimizers, callbacks
-from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
-from tensorflow_examples.models.pix2pix import pix2pix
-from sklearn.model_selection import train_test_split
-from IPython.display import clear_output
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-from scipy import io
-import glob, os
-#%%
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers,losses
+from tensorflow_examples.models.pix2pix import pix2pix
+from tensorflow.keras.utils import plot_model
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 # Initialize empty lists for images and masks from the datasets
 images = []
 masks=[]
 PATH = r'C:\Users\User\OneDrive\Documents\GitHub\ai07-nuclei-semantic-segmentation\datasets\train'
 
-#%%
 # Load images from datasets using opencv
 IMG_PATH = os.path.join(PATH,'inputs')
 for image_file in os.listdir(IMG_PATH):
@@ -32,17 +29,16 @@ for image_file in os.listdir(IMG_PATH):
     img = cv2.resize(img,(128,128))
     images.append(img)
 
-#%%
 # Load masks from datasets
 MASK_PATH = os.path.join(PATH,'masks')
 for mask_file in os.listdir(MASK_PATH):
     mask = cv2.imread(os.path.join(MASK_PATH,mask_file),cv2.IMREAD_GRAYSCALE)
     mask = cv2.resize(mask,(128,128))
     masks.append(mask)
-#%%
+
 images_np  = np.array(images)
 masks_np = np.array(masks)
-#%%
+
 # display examples of images from datasets
 plt.figure(figsize=(10,10))
 for i in range(1,4):
@@ -55,7 +51,7 @@ for i in range(1,4):
     plt.imshow(masks_np[i])
     plt.axis('off')
 plt.show()
-#%% 
+ 
 # Data pre-processing
 
 # Add channel 
@@ -63,38 +59,38 @@ mask_np_exp = np.expand_dims(masks_np,axis=-1)
 print(f"Unique Masks : {np.unique(masks[0])}")
 print(f"Masks expanded shape :{mask_np_exp.shape}")
 
-#%% 
+ 
 cvt_masks = np.round(mask_np_exp/255).astype(np.int64)
 print(f"Unique mask converted :{np.unique(cvt_masks[0])}")
-#%%
+
 cvt_images = images_np/255.0
 sample_img = cvt_images[0]
 
-#%%
+
 # Train test split
 SEED = 12345
 X_train, X_test, y_train, y_test = train_test_split(cvt_images,cvt_masks,test_size=0.2,random_state=SEED)
-#%%
+
 # Convert arrays into tensors
 x_train_tensor = tf.data.Dataset.from_tensor_slices(X_train) 
 x_test_tensor =tf.data.Dataset.from_tensor_slices(X_test)
 y_train_tensor = tf.data.Dataset.from_tensor_slices(y_train)
 y_test_tensor = tf.data.Dataset.from_tensor_slices(y_test)
-#%%
+
 # Combine images and masks
 train_dataset = tf.data.Dataset.zip((x_train_tensor,y_train_tensor))
 test_dataset=  tf.data.Dataset.zip((x_test_tensor,y_test_tensor))
-#%%
+
 class Augment(layers.Layer):
     def __init__(self,seed=42):
         super().__init__()
         self.augment_inputs = layers.RandomFlip(mode='horizontal',seed=seed)
-        self.augment_labels = layers.RandomFlip(mode='horizontal',seed=seed)
+        self.augment_labels = layers.RandomFlip(mode='vertical',seed=seed)
     def call(self,inputs,labels):
         inputs = self.augment_inputs(inputs)
         labels = self.augment_labels(labels)
         return inputs,labels
-#%%
+
 # Convert dataset into prefetch dataset
 BATCH_SIZE = 32
 AUTOTUNE = tf.data.AUTOTUNE
@@ -110,7 +106,7 @@ train_batches = (train_dataset
                  .map(Augment())
                  .prefetch(buffer_size=tf.data.AUTOTUNE))
 test_batches = test_dataset.batch(BATCH_SIZE)
-#%%
+
 # Visualize images and masks side-by-side
 def display(display_list):
     plt.figure(figsize=(15,15))
@@ -123,14 +119,11 @@ def display(display_list):
         plt.axis('off')
         
     plt.show()
-    
-#%%
+
 for images, masks in train_batches.take(2):
     sample_image,sample_mask = images[0],masks[0]
     display([sample_image,sample_mask])
-    
 
-#%%
 # Create image segmentation model
 
 # Transfer learning (MOBILENET)
@@ -145,7 +138,7 @@ layer_names = [
     'block_16_project',      # 4x4
 ]
 base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
-#%%
+
 # Upsampling path
 up_stack = [
     pix2pix.upsample(512, 3),  # 4x4 -> 8x8
@@ -153,7 +146,7 @@ up_stack = [
     pix2pix.upsample(128, 3),  # 16x16 -> 32x32
     pix2pix.upsample(64, 3),   # 32x32 -> 64x64
     ]
-#%%
+
 down_stack = keras.Model(inputs=base_model.input, outputs=base_model_outputs)
 down_stack.trainable = False
 def unet_model(output_channels:int):
@@ -179,16 +172,19 @@ def unet_model(output_channels:int):
     
     return keras.Model(inputs=inputs, outputs=x)
 
-#%%
 # Construct UNET with MobileNetV2 as the encoder
 OUTPUT_CLASSES = 3
 model = unet_model(output_channels=OUTPUT_CLASSES)
-#%%
+
+# Save structure of model
+img_file = 'public/model_plot.png'
+plot_model(model, to_file=img_file, show_shapes=True)
+
 # Compile Model
 loss = losses.SparseCategoricalCrossentropy(from_logits=True)
 model.compile(optimizer='adam',loss=loss,metrics=['accuracy'])
 keras.utils.plot_model(model, show_shapes=True)
-#%%
+
 # Functions to create masks and show predictions
 def create_mask(pred_mask):
     pred_mask = tf.argmax(pred_mask,axis=-1)
@@ -203,10 +199,10 @@ def show_predictions(dataset=None,num=1):
             
     else:
         display([sample_image,sample_mask,create_mask(model.predict(sample_image[tf.newaxis,...]))])
-#%%
+
 # Show predictions
 show_predictions()
-#%%
+
 # Train Model
 #Hyperparameters for the model
 EPOCHS = 10
@@ -214,7 +210,6 @@ VAL_SUBSPLITS = 5
 VAL_STEPS = len(test_dataset)//BATCH_SIZE//VAL_SUBSPLITS
 history = model.fit(train_batches,validation_data=test_batches,epochs=EPOCHS,steps_per_epoch=STEPS_PER_EPOCH,validation_steps=VAL_STEPS)
 
-#%%
 # Deploy model
 print("==================MODEL INFERENCE================================")
 show_predictions(test_batches,3)
